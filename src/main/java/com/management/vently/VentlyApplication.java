b54aeeef95e54a4841803392eb954f22f868bdea
alojine <76651648+alojine@users.noreply.github.com>
2023-08-09 14:19:55 +0300

    first commit

---
+++ src/main/java/com/management/vently/VentlyApplication.java
@@ -0,0 +1,13 @@
+package com.management.vently;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/management/vently/VentlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.vently;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
