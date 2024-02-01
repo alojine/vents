d10a31a5d5865d72d1c9235c859f26bface106b6
alojine <benaspaliokas@gmail.com>
2024-02-01 17:58:04 +0200

    catching exceptions, improved auth system

--- src/main/java/com/management/vently/exception/GlobalExceptionHandler.java
+++ src/main/java/com/management/vently/exception/GlobalExceptionHandler.java
@@ -3,12 +3,12 @@ package com.management.vently.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
 
-@Controller
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
@@ -21,5 +21,17 @@ public class GlobalExceptionHandler {
         );
 
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RequestValidationException.class)
+    public ResponseEntity<VentlyApiError> handleRequestValidationException(RequestValidationException ex, HttpServletRequest request) {
+        VentlyApiError error = new VentlyApiError(
+                request.getRequestURI(),
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/management/vently/service/Impl/AuthenticationServiceImpl.java
+++ src/main/java/com/management/vently/service/Impl/AuthenticationServiceImpl.java
@@ -3,12 +3,14 @@ package com.management.vently.service.Impl;
 import com.management.vently.domain.DTO.*;
 import com.management.vently.enums.Role;
 import com.management.vently.domain.model.User;
-import com.management.vently.repository.UserRepository;
+import com.management.vently.exception.NotFoundException;
+import com.management.vently.exception.RequestValidationException;
 import com.management.vently.service.AuthenticationService;
 import com.management.vently.service.JwtService;
 import com.management.vently.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -35,9 +37,10 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 .role(Role.USER)
                 .build();
 
-        if (userService.getByEmail(request.email()).isEmpty()) {
-            throw new
-        }
+        userService.getByEmail(request.email()).ifPresent(existingUser -> {
+            throw new RequestValidationException(String.format("User with email: %s already exists", existingUser.getEmail()));
+        });
+
 
         userService.save(user);
         var jwtToken = jwtService.generateToken(user);
@@ -46,14 +49,19 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public AuthResponse authenticate(AuthRequest request) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.email(),
-                        request.password()
-                )
-        );
-        // TODO user service instead of repository
-        var user = userService.getByEmail(request.email());
+        User user = userService.getByEmail(request.email())
+                .orElseThrow(() -> new NotFoundException(String.format("User with email: %s does not exist", request.email())));
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.email(),
+                            request.password()
+                    )
+            );
+        } catch (BadCredentialsException e) {
+            throw new NotFoundException(String.format("Invalid credentials for user with email: %s", request.email()));
+        }
 
         var jwtToken = jwtService.generateToken(user);
         return new AuthResponse(jwtToken);
