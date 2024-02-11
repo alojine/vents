8337144a117a08dd598490afaa22f9202044c47b
alojine <benaspaliokas@gmail.com>
2024-02-11 21:25:31 +0200

    PasswordController

--- src/main/java/com/management/vently/controller/PasswordController.java
+++ src/main/java/com/management/vently/controller/PasswordController.java
@@ -1,16 +1,13 @@
 package com.management.vently.controller;
 
+import com.management.vently.domain.DTO.PasswordUpdateDto;
 import com.management.vently.mapper.PasswordMapper;
 import com.management.vently.domain.DTO.PasswordDTO;
-import com.management.vently.domain.model.Password;
 import com.management.vently.service.PasswordService;
-import com.management.vently.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/password")
@@ -18,14 +15,35 @@ public class PasswordController {
 
     private final PasswordService passwordService;
 
-    private final UserService userService;
-
     private final PasswordMapper passwordMapper;
 
     @Autowired
-    public PasswordController(PasswordService passwordService, UserService userService, PasswordMapper passwordMapper) {
+    public PasswordController(PasswordService passwordService, PasswordMapper passwordMapper) {
         this.passwordService = passwordService;
-        this.userService = userService;
         this.passwordMapper = passwordMapper;
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PasswordDTO> getPasswordById(@PathVariable Long id) {
+        return new ResponseEntity<>(passwordMapper.passwordToPasswordDTO(passwordService.getById(id)), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<HttpStatus> createPassword(@RequestBody PasswordDTO passwordDTO) {
+        passwordService.add(passwordMapper.passwordDTOtoPassword(passwordDTO), passwordDTO.userId());
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<HttpStatus> updatePassword(@RequestBody PasswordUpdateDto passwordDTO) {
+        passwordService.update(passwordDTO, passwordDTO.userId());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteNote(@PathVariable Long id) {
+        passwordService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/management/vently/domain/DTO/PasswordDTO.java
+++ src/main/java/com/management/vently/domain/DTO/PasswordDTO.java
@@ -1,7 +1,5 @@
 package com.management.vently.domain.DTO;
 
-import com.management.vently.domain.model.User;
-
 public record PasswordDTO(
         Long userId,
         String applicationName,
