a1fc3ebb66493594fbba53f5a62a9f97c18de9e0
alojine <benaspaliokas@gmail.com>
2024-02-12 10:00:25 +0200

    Fixed rest naming

--- api/src/main/java/com/management/vently/controller/PasswordController.java
+++ api/src/main/java/com/management/vently/controller/PasswordController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/password")
+@RequestMapping("api/v1/passwords")
 public class PasswordController {
 
     private final PasswordService passwordService;
